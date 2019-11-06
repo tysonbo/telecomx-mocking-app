a3aab03cede86d84e803081b01e0d20d5a9465bb
jgrucza <jay-g6@outlook.com>
2019-11-06 14:36:04 -0500

    added bootstrap and customers

---
+++ src/app/data/customerData.ts
@@ -0,0 +1,14 @@
+import { Customer } from './customer';
+export let customerData: Array<Customer> = [
+    // tslint:disable-next-line:max-line-length
+    new Customer('12345678', 'Mickey Mouse', '1981-10-18', '400 Disney Way', 
+    'Orlando', 'FL', '44137', '333-333-3333', 'mickey@outlook.com'),
+    new Customer('12477678', 'Donal Duck', '1981-10-18', '545 Disney Pond', 
+    'Orlando', 'FL', '44137', '333-333-3333', 'mickey@outlook.com'),
+    new Customer('22375678', 'Roy Rogers', '1981-10-18', '600 Western Blvd', 
+    'Arizona', 'AZ', '44137', '333-333-3333', 'mickey@outlook.com'),
+    new Customer('31235578', 'Babe Ruth', '1981-10-18', '7745 HomeRun Way', 
+    'New York', 'NY', '44137', '333-333-3333', 'mickey@outlook.com'),
+    new Customer('64445678', 'Fred Flintstone', '1981-10-18', '4545 Bedrock Avenue', 
+'Bedrock', 'CO', '44137', '333-333-3333', 'mickey@outlook.com')
+];
